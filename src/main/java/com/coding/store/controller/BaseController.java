f6d9fdf2113310a9a8d3796f26f2ee2bc19a8147
Aslam <tweakystiliton@gmail.com>
2025-10-17 17:39:05 +1030

    full-stack

--- src/main/java/com/coding/store/controller/BaseController.java
+++ src/main/java/com/coding/store/controller/BaseController.java
@@ -3,7 +3,6 @@ package com.coding.store.controller;
 import com.coding.store.model.Product;
 import com.coding.store.repository.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -27,14 +26,6 @@ public class BaseController {
     @GetMapping("/AllProducts")
     public List<Product> AllProducts() {
         return repo.findAll();
-    }
-    public String row(Product product) {
-        String sold;
-        if(product.getSold())
-            sold = "Available";
-        else
-            sold = "not Available";
-        return "<div style = 'background: white; color: MediumSeaGreen; font-size: 20px; padding: 10px; margin: 8px;'><label> <span style = 'background: lightgreen'> Item: </span> </label>" + product.getItem() + "<label><span style = 'background: blue'> Cost: </span>" + product.getCost() + "</label>"  + "<label><span style = 'background: green'> Sold: </span>" +  sold + "</label> </div>" ;
     }
     @CrossOrigin("http://localhost:3000/")
     @DeleteMapping("/DeleteProduct/{id}")
